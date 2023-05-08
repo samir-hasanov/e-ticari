59541216e206983ea77885f3fc670a190e8635cc
Samir Hasanov <samirhasanov18@gmail.com>
2023-05-08 11:56:49 +0400

    create new e-ticri prj

---
+++ src/main/java/electronic/commerce/ETicariApplication.java
@@ -0,0 +1,13 @@
+package electronic.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETicariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ETicariApplication.class, args);
+	}
+
+}
---
+++ src/test/java/electronic/commerce/ETicariApplicationTests.java
@@ -0,0 +1,13 @@
+package electronic.commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETicariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
