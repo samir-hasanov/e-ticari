5b259d2803899df9397e8dc94b5e4631a18fee6d
Samir Hasanov <samirhasanov18@gmail.com>
2023-05-09 12:36:30 +0400

    created exception constants

---
+++ src/main/java/electronic/commerce/exception/ExceptionConstants.java
@@ -0,0 +1,10 @@
+package electronic.commerce.exception;
+
+public class ExceptionConstants {
+    public final static Integer INTERNAL_EXCEPTION=100;
+    public final static Integer DATA_NULL=101;
+    public final static Integer INVALID_REQUEST=102;
+    public final static Integer NOT_FOUND=103;
+    public final static Integer DATA_TAKEN=104;
+
+}
